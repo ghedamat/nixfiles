f1b7500aa9bf02ec754057e8e48f137e44793a8d
Mattia Gheda <ghedamat@gmail.com>
2019-07-01 11:32:36 -0400

    git config ported

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -10,4 +10,74 @@
   home.file.".tmux.conf".source = config/tmux.conf;
   home.file.".xinitrc".source = config/X/xinitrc;
 
+
+  programs.git = {
+    enable = true;
+    userName = "Mattia Gheda";
+    userEmail = "ghedamat@gmail.com";
+    aliases = {
+      st = "status";
+      s = "status --short";
+      ci = "commit";
+      br = "branch";
+      co = "checkout";
+      ff = "merge --ff-only";
+      df = "diff";
+      lg = "log -p";
+      edit-unmerged = "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; emacs `f`";
+      add-unmerged = "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; git add `f`";
+      clone = "clone --recursive";
+    };
+    extraConfig = {
+      core = {
+        editor = "vim";
+        whitespace = "trailing-space,space-before-tab";
+        excludesfile = "/home/ghedamat/.gitignore_global";
+      };
+      push = {
+        default = "tracking";
+      };
+      merge = {
+        keepBackup = false;
+        tool = "custom";
+      };
+      github = {
+        user = "ghedamat";
+      };
+      color = {
+        branch = "auto";
+        diff = "auto";
+        status = "auto";
+      };
+      "color \"branch\"" = {
+        current = "yellow reverse";
+        local = "yellow";
+        remote = "green";
+      };
+      "color \"diff\"" = {
+        meta = "yellow bold";
+        frag = "magenta bold";
+        old = "red bold";
+        new = "green bold";
+      };
+      "color \"status\"" = {
+        added = "yellow";
+        changed = "green";
+        untracked = "cyan";
+      };
+    };
+  };
+
+  #gtk = {
+  #  enable = true;
+  #  theme = {
+  #    package = pkgs.theme-vertex;
+  #    name = "Vertex-Dark";
+  #  };
+  #  iconTheme = {
+  #    package = pkgs.tango-icon-theme;
+  #    name = "Tango";
+  #  };
+  #};
+
 }
