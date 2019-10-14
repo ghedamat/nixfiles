bedf14b74fadbda7f5c780ca949a70109994906c
Mattia Gheda <ghedamat@gmail.com>
2019-10-13 21:01:50 -0400

    xorg retry

--- hardware/desktop-i5.nix
+++ hardware/desktop-i5.nix
@@ -32,10 +32,12 @@
     DefaultDepth    24
     Option         "Stereo" "0"
     Option         "nvidiaXineramaInfoOrder" "DFP-1"
-    Option         "metamodes" "DVI-D-0: nvidia-auto-select +3840+0, HDMI-0: nvidia-auto-select +1920+1080, DP-2: nvidia-auto-select +0+0, DP-0: 1920x1080 +1920+0; DVI-D-0: nvidia-auto-select +0+0, DP-0: nvidia-auto-select +0+0; DVI-D-0: 1920x1080 +0+0, DP-0: nvidia-auto-select +0+0; DVI-D-0: 1680x1050 +0+0, DP-0: nvidia-auto-select +0+0; DVI-D-0: 1600x1200 +0+0, DP-0: nvidia-auto-select +0+0; DVI-D-0: 1280x1024 +0+0, DP-0: nvidia-auto-select +0+0; DVI-D-0: 1280x960 +0+0, DP-0: nvidia-auto-select +0+0; DVI-D-0: 1024x768 +0+0, DP-0: nvidia-auto-select +0+0; DVI-D-0: 800x600 +0+0, DP-0: nvidia-auto-select +0+0; DVI-D-0: 640x480 +0+0, DP-0: nvidia-auto-select +0+0; DVI-D-0: nvidia-auto-select +0+0 {viewportin=1440x900}, DP-0: nvidia-auto-select +0+0; DVI-D-0: nvidia-auto-select +0+0 {viewportin=1366x768, viewportout=1920x1079+0+60}, DP-0: nvidia-auto-select +0+0"
+    Option         "metamodes" "DVI-D-0: nvidia-auto-select +3840+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, HDMI-0: nvidia-auto-select +1920+1080 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, DP-2: nvidia-auto-select +0+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, DP-0: 1920x1080 +1920+0; DVI-D-0: nvidia-auto-select +3840+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, DP-0: nvidia-auto-select +0+0; DVI-D-0: 1920x1080 +3840+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, DP-0: nvidia-auto-select +0+0; DVI-D-0: 1680x1050 +3840+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, DP-0: nvidia-auto-select +0+0; DVI-D-0: 1600x1200 +3840+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, DP-0: nvidia-auto-select +0+0"
     Option         "SLI" "Off"
     Option         "MultiGPU" "Off"
     Option         "BaseMosaic" "off"
+    Option         "AllowIndirectGLXProtocol" "off"
+    Option         "TripleBuffer" "on"
     SubSection     "Display"
         Depth       24
     EndSubSection
