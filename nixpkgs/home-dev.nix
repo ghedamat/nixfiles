80aada3b443e35399a39feca07bb022938634bd8
Mattia Gheda <ghedamat@gmail.com>
2019-11-16 15:51:57 +0000

    dry dev config

--- nixpkgs/home-dev.nix
+++ nixpkgs/home-dev.nix
@@ -7,66 +7,22 @@
 
   home.packages = with pkgs; [
     vscode
+    homesick
+    fzf
   ];
 
   imports = [
     ./config/tmux.conf.nix
+    ./config/git.nix
+    ./config/zsh.nix
   ];
 
-  programs.git = {
-    enable = true;
-    userName = "Mattia Gheda";
-    userEmail = "ghedamat@gmail.com";
-    aliases = {
-      st = "status";
-      s = "status --short";
-      ci = "commit";
-      br = "branch";
-      co = "checkout";
-      ff = "merge --ff-only";
-      df = "diff";
-      lg = "log -p";
-      edit-unmerged = "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; emacs `f`";
-      add-unmerged = "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; git add `f`";
-      clone = "clone --recursive";
+  programs.zsh = {
+    oh-my-zsh = {
+      theme = "minimal";
     };
-    extraConfig = {
-      core = {
-        editor = "vim";
-        whitespace = "trailing-space,space-before-tab";
-        excludesfile = "/home/ghedamat/.gitignore_global";
-      };
-      push = {
-        default = "tracking";
-      };
-      merge = {
-        keepBackup = false;
-        tool = "custom";
-      };
-      github = {
-        user = "ghedamat";
-      };
-      color = {
-        branch = "auto";
-        diff = "auto";
-        status = "auto";
-      };
-      "color \"branch\"" = {
-        current = "yellow reverse";
-        local = "yellow";
-        remote = "green";
-      };
-      "color \"diff\"" = {
-        meta = "yellow bold";
-        frag = "magenta bold";
-        old = "red bold";
-        new = "green bold";
-      };
-      "color \"status\"" = {
-        added = "yellow";
-        changed = "green";
-        untracked = "cyan";
-      };
-    };
+    profileExtra = ''
+      export PATH=$PATH:$HOME/.npm-prefix/bin
+    '';
   };
 }
