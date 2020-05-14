7dfaf4eb3de9d24ef415837c5ec7199a42277dc6
Mattia Gheda <ghedamat@gmail.com>
2020-05-14 15:42:53 +0000

    configure xorg for dev server

--- common/server.nix
+++ common/server.nix
@@ -22,5 +22,9 @@
     enable = true;
     layout = "us";
     libinput.enable = true;
+    displayManager.startx.enable = true;
+    displayManager.defaultSession = "none+i3";  # We startx in our home.nix
+
+    windowManager.i3.enable = true;
   };
 }
--- hive-lurker-devserver.nix
+++ hive-lurker-devserver.nix
@@ -96,6 +96,9 @@
 
   services.qemuGuest.enable = true;
 
+  xdg.portal.enable = true;
+  services.flatpak.enable = true;
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
