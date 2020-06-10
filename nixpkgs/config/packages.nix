c8f9fcf4136de8aca8ccb65b1524d9b54f0ad9ad
Mattia <ghedamat@gmail.com>
2020-06-10 16:08:40 -0400

    update vscode

--- nixpkgs/config/packages.nix
+++ nixpkgs/config/packages.nix
@@ -2,12 +2,12 @@
 let unstable = import <unstable> { };
 in {
   home.packages = with pkgs; [
-    vscode
     homesick
     fzf
     meld
     unstable.starship
     pass
     (import ./../../common/packages/neovim.nix)
+    unstable.vscode
   ];
 }
