7c589cc2b51b9619e67c442d59c8b36653e5e46a
Mattia Gheda <ghedamat@gmail.com>
2020-06-28 23:50:03 +0000

    use lightdm on all systems

--- common/server.nix
+++ common/server.nix
@@ -17,9 +17,5 @@
     enable = true;
     layout = "us";
     libinput.enable = true;
-    displayManager.startx.enable = true;
-    displayManager.defaultSession = "none+i3"; # We startx in our home.nix
-
-    windowManager.i3.enable = true;
   };
 }
