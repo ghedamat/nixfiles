5f67f174e5018c868af4a3436279b12fc4a5a4a5
Mattia Gheda <ghedamat@gmail.com>
2020-08-04 15:56:15 +0000

    tweaks

--- hive-lurker-devserver.nix
+++ hive-lurker-devserver.nix
@@ -38,7 +38,7 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [ qemu ];
+  environment.systemPackages = with pkgs; [ qemu google-chrome ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
