43735ac472070a617904e375d2ad535b1c0ec232
Mattia <ghedamat@gmail.com>
2020-09-05 00:11:50 -0400

    desktop module

--- nixpkgs/home-desktop.nix
+++ nixpkgs/home-desktop.nix
@@ -6,31 +6,49 @@
   nixpkgs.config.allowUnfree = true;
 
   imports = [
-    ./config/git.nix
-    ./config/zsh.nix
+    ./config/shell.nix
     ./config/programs.nix
+    ./config/git.nix
     ./config/packages.nix
-    ./config/services.nix
-    ./xfiles/xfiles-hydralisk.nix
+    ./config/spacevim.nix
+    ./config/xfiles.nix
   ];
+
   home.packages = with pkgs;
     [
       #zoom-us
     ];
 
-  home.file.".config/tmux.conf".source = ./config/tmux.conf;
   home.file."/bin/focus.sh".source = ./bin/focus.sh;
   home.file."/bin/colorterm.sh".source = ./bin/colorterm.sh;
-  home.file.".config/i3/config".source = ./config/i3/config-desktop;
-  home.file.".config/alacritty.yml".source = ./config/alacritty-hydralisk.yml;
-  home.file.".config/i3/status.toml".source = ./config/i3/status-desktop.toml;
+  ghedamat = {
 
-  programs.zsh = {
-    oh-my-zsh = { theme = "minimal"; };
-    profileExtra = ''
-      export PATH=$PATH:$HOME/.npm-prefix/bin
-    '';
-    initExtraBeforeCompInit = "";
+    shell = {
+      zsh = {
+        enable = true;
+      };
+    };
+
+    programs = {
+      tmux.enable = true;
+      alacritty = {
+        enable = true;
+        configFile = "hydralisk.yml";
+      };
+      i3 = {
+        enable = true;
+        configFile = "config-desktop";
+        statusFile = "status-desktop.toml";
+      };
+    };
+
+    spacevim.enable = true;
+
+    xfiles = {
+      enable = true;
+      xinitrcFile = "xinitrc-hydralisk";
+      xprofileFile = "xprofile-hydralisk";
+    };
   };
 
   gtk = {
