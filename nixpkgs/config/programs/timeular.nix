01afd69566c30e94f02d5c32be5048b7a4439698
Mattia Gheda <ghedamat@gmail.com>
2020-10-04 21:43:46 -0400

    backport timeular for stable

--- nixpkgs/config/programs/timeular.nix
+++ nixpkgs/config/programs/timeular.nix
@@ -3,7 +3,12 @@
 with lib;
 let cfg = config.ghedamat.programs.timeular;
 in {
-  options = { ghedamat.programs.timeular.enable = mkEnableOption "enable timeular"; };
-  config = mkIf cfg.enable { home.packages = with pkgs; [ timeular ]; };
+  options = {
+    ghedamat.programs.timeular.enable = mkEnableOption "enable timeular";
+  };
+  config = mkIf cfg.enable {
+    home.packages = with pkgs;
+      [ (import ./../../../common/packages/timeular-backport.nix) ];
+  };
 }
 
