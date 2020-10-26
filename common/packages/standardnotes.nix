cf05f24c6d375bc7e5b71421b76cc03f53f9186b
Mattia Gheda <ghedamat@gmail.com>
2020-10-26 18:58:57 -0400

    sn 3.5.0-beta5 and gtk support

--- common/packages/standardnotes.nix
+++ common/packages/standardnotes.nix
@@ -1,6 +1,6 @@
 with import <nixpkgs> { };
 let
-  version = "3.5.1-beta03";
+  version = "3.5.1-beta05";
   pname = "standardnotes";
   name = "${pname}-${version}";
 
@@ -11,26 +11,32 @@ let
 
   sha256 = {
     i386-linux = "0v2nsis6vb1lnhmjd28vrfxqwwpycv02j0nvjlfzcgj4b3400j7a";
-    x86_64-linux = "0przgmhykz9c28pkazbqh7j80xnvkldgdgjmzxwc8idcqgwb3bs4";
+    x86_64-linux = "1f0z4p1did4jizi0gwx6qp42vp59rmxfphjwh1j83w1whh1pa45f";
   }.${stdenv.hostPlatform.system};
 
+  xdg_dirs = builtins.concatStringsSep ":"
+    [ "${gtk3}/share/gsettings-schemas/${gtk3.name}" ];
+
   src = fetchurl {
-    url = "https://github.com/standardnotes/desktop/releases/download/v${version}/standard-notes-${version}-linux-${plat}.AppImage";
+    url =
+      "https://github.com/standardnotes/desktop/releases/download/v${version}/standard-notes-${version}-linux-${plat}.AppImage";
     inherit sha256;
   };
 
-  appimageContents = appimageTools.extractType2 {
-    inherit name src;
-  };
+  appimageContents = appimageTools.extractType2 { inherit name src; };
 
   nativeBuildInputs = [ autoPatchelfHook desktop-file-utils ];
 
 in appimageTools.wrapType2 rec {
   inherit name src;
 
-  extraPkgs = pkgs: with pkgs; [
-    libsecret
-  ];
+  extraPkgs = pkgs:
+    with pkgs; [
+      libsecret
+      wrapGAppsHook
+      gtk3
+      hicolor-icon-theme
+    ];
 
   extraInstallCommands = ''
     # directory in /nix/store so readonly
@@ -42,6 +48,10 @@ in appimageTools.wrapType2 rec {
     ${desktop-file-utils}/bin/desktop-file-install --dir $out/share/applications \
       --set-key Exec --set-value ${pname} standard-notes.desktop
     rm usr/lib/* AppRun standard-notes.desktop .so*
+  '';
+
+  profile = ''
+    export XDG_DATA_DIRS="${xdg_dirs}''${XDG_DATA_DIRS:+:"$XDG_DATA_DIRS"}"
   '';
 
   meta = with stdenv.lib; {
