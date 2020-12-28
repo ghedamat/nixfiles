7bb33200177497e89760d7eea74b9f2ac4a85470
Mattia Gheda <ghedamat@gmail.com>
2020-12-28 22:46:59 +0000

    Revert "temporarely remove nixops due to bug in unstable"
    This reverts commit 42399da0884d6f76929fd0f56e4f87fa000b8ddb.

--- common/hivemind/server.nix
+++ common/hivemind/server.nix
@@ -8,7 +8,7 @@ in {
     hivemind.server.xserver = mkEnableOption "enable xserver";
   };
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ gnumake git ];
+    environment.systemPackages = with pkgs; [ gnumake git nixops ];
 
     networking.networkmanager.enable = true;
     programs.mosh.enable = true;
