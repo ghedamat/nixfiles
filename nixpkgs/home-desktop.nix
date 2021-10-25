2a7f6066121ad8f6056f75ad43465698be79a020
Mattia Gheda <ghedamat@gmail.com>
2021-10-25 14:25:11 -0400

    fix path

--- nixpkgs/home-desktop.nix
+++ nixpkgs/home-desktop.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 
-let unstable = (import <unstable> { });
+let unstable = (import <nixpkgs> { });
 in {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
