3078203db65b591f9035e79abfbcaf47ed738e4a
Mattia Gheda <ghedamat@gmail.com>
2022-01-13 16:04:26 +0000

    enable flakes on drone

--- hive-drone-devserver.nix
+++ hive-drone-devserver.nix
@@ -11,6 +11,14 @@
     ./common/hivemind.nix
   ];
 
+  # enable flakes
+  nix = {
+    package = pkgs.nixUnstable; # or versioned attributes like nix_2_4
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
